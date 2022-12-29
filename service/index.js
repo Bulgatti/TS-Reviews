a94a559ae2fbccaa0e218c694d6bf10928e781cb
Jordan Searcy-Hosea <o7endofwatch@mailfence.com>
2022-12-29 12:02:02 -1000

    Initial commit

---
+++ service/index.js
@@ -0,0 +1,32 @@
+const cluster = require('cluster');
+const { cpus } = require('os');
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const router = require('./routes');
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} running`);
+
+  const systemCPUs = cpus().length;
+  for (let i = 0; i < systemCPUs; i += 1) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', worker => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  const app = express();
+  app.use(cors());
+  app.use(morgan('dev'));
+  app.use(router);
+
+  const PORT = process.env.PORT ?? 1337;
+  app.listen(PORT, () => {
+    console.log(`Worker ${process.pid} listening at http://localhost:${PORT}`);
+  });
+}
