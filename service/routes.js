29bf40f228318b22b101a8f73546bdf1e6134d67
Jordan Searcy-Hosea <o7endofwatch@mailfence.com>
2022-12-30 05:41:14 -1000

    Add `Reviews` routes

--- service/routes.js
+++ service/routes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
+const { Reviews } = require('./controllers');
+
+router.get('/reviews', Reviews.getReviews);
+router.post('/reviews', Reviews.addReview);
+router.put('/reviews/:review_id/helpful', Reviews.updateReview);
+router.put('/reviews/:review_id/report', Reviews.updateReview);
 
 module.exports = router;
