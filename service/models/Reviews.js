5973876721168711acf3e8e2fe94cab533808a72
Jordan Searcy-Hosea <o7endofwatch@mailfence.com>
2022-12-30 05:40:45 -1000

    Implement `Reviews` model

---
+++ service/models/Reviews.js
@@ -0,0 +1,29 @@
+const { Reviews } = require('../db');
+
+module.exports = {
+  getReviews: (product, options) => {
+    try {
+      return Promise.resolve(Reviews.find({ product_id: product, reported: false })
+        .skip((options.page - 1) * 5)
+        .limit(options.count)
+        .sort(options.sort)
+        .lean());
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+  addReview: review => {
+    try {
+      return Promise.resolve(Reviews.create(review));
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+  updateReview: (review, update) => {
+    try {
+      return Promise.resolve(Reviews.updateOne(review, update));
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+};
