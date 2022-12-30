6bf7c76df64382a6a0ba753ba1309cc4950c2105
Jordan Searcy-Hosea <o7endofwatch@mailfence.com>
2022-12-29 18:18:53 -1000

    Move router into worker scope

--- service/index.js
+++ service/index.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const router = require('./routes');
 
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} running`);
@@ -20,6 +19,10 @@ if (cluster.isPrimary) {
     cluster.fork();
   });
 } else {
+  /** Router needs to be moved into worker scope to prevent primary from accessing it */
+  // eslint-disable-next-line global-require
+  const router = require('./routes');
+
   const app = express();
   app.use(cors());
   app.use(morgan('dev'));
