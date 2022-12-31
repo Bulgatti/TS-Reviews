451929ce8efbaebf0ae50e4e99d408b283d4a35c
Jordan Searcy-Hosea <o7endofwatch@mailfence.com>
2022-12-31 10:52:22 -1000

    Export `ProductChars` and `Chars` models

--- service/models/index.js
+++ service/models/index.js
@@ -1,2 +1,4 @@
 module.exports.Reviews = require('./Reviews');
 module.exports.Photos = require('./Photos');
+module.exports.ProductChars = require('./ProductChars');
+module.exports.Chars = require('./Chars');
