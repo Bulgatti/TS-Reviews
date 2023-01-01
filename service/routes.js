a042e8e6f73fb477b27f5247f16ba7d285d395b5
Jordan Searcy-Hosea <o7endofwatch@mailfence.com>
2023-01-01 13:27:56 -1000

    Add `/reviews/meta` route

--- service/routes.js
+++ service/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Reviews } = require('./controllers');
 
 router.get('/reviews', Reviews.getReviews);
+router.get('/reviews/meta', Reviews.getMetadata);
 router.post('/reviews', Reviews.addReview);
 router.put('/reviews/:review_id/helpful', Reviews.updateReview);
 router.put('/reviews/:review_id/report', Reviews.updateReview);
